9bb981591a72f00a22d37b793a350e8d5f974a3a
Adam School <adrodgers@my365.bellevue.edu>
2022-03-26 15:07:19 -0500

    Updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,44 @@
-import { HomeComponent } from './pages/home/home.component';
-import { BaseLayoutComponent } from './shared/base-layout/base-layout.component';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from "./pages/home/home.component";
+import { BaseLayoutComponent } from "./shared/base-layout/base-layout.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { SignInGuard } from "./shared/sign-in.guard";
+import { AuthLayoutComponent } from "./shared/auth-layout/auth-layout.component";
+import { SignInComponent } from "./pages/sign-in/sign-in.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: BaseLayoutComponent,
     children: [
       {
-        path: '',
-        component: HomeComponent
-      }
-    ]
-  }
+        path: "",
+        component: HomeComponent,
+        canActivate: [SignInGuard],
+      },
+      {
+        path: "home",
+        component: HomeComponent,
+        canActivate: [SignInGuard],
+      },
+    ],
+  },
+  {
+    path: "session",
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: "signin",
+        component: SignInComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true, enableTracing: false, scrollPositionRestoration: 'enabled', relativeLinkResolution: 'legacy' })],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, { useHash: true, enableTracing: false, scrollPositionRestoration: "enabled", relativeLinkResolution: "legacy" }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { CookieService } from "ngx-cookie-service";
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  selector: "app-sign-in",
+  templateUrl: "./sign-in.component.html",
+  styleUrls: ["./sign-in.component.css"],
 })
 export class SignInComponent implements OnInit {
+  form: FormGroup;
+  error: string;
 
-  constructor() { }
+  constructor(private router: Router, private cookieService: CookieService, private fb: FormBuilder, private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      empId: [null, Validators.compose([Validators.required, Validators.pattern("^[0-9]*$")])],
+    });
   }
+  login() {
+    const empId = this.form.controls["empId"].value;
 
+    this.http.get("/api/employees" + empId).subscribe((res) => {
+      if (res) {
+        this.cookieService.set("session_user", empId, 1);
+        this.router.navigate(["/"]);
+      } else {
+        this.error = "You enter an invalid employee ID";
+      }
+    });
+  }
 }
