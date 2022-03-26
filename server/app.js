4b2ffa99deee23f2be43109f4aea228c97ccfc6e
Adam School <adrodgers@my365.bellevue.edu>
2022-03-26 12:52:28 -0500

    Updates

--- server/app.js
+++ server/app.js
@@ -1,51 +1,64 @@
 /**
  * Require statements
  */
-const express = require('express');
-const http = require('http');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');                        
-const path = require('path');
-const mongoose = require('mongoose');
+const express = require("express");
+const http = require("http");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const path = require("path");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 /**
  * App configurations
  */
 let app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({'extended': true}));
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, '../dist/nodebucket')));
-app.use('/', express.static(path.join(__dirname, '../dist/nodebucket')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "../dist/nodebucket")));
+app.use("/", express.static(path.join(__dirname, "../dist/nodebucket")));
 
 /**
  * Variables
  */
-const port = 3000; // server port
+const port = process.env.PORT || 3000; // server port
 
-// TODO: This line will need to be replaced with your actual database connection string
-const conn = 'mongodb+srv://superadmin:s3cret@cluster0-lujih.mongodb.net/nodebucket?retryWrites=true&w=majority';
+// Using NPM package 'dotenv' to avoid exposing username/password on Github
+const conn =
+  "mongodb+srv://" +
+  process.env.ATLAS_USER +
+  ":" +
+  process.env.ATLAS_PW +
+  "@buwebdev-cluster-1.zjoha.mongodb.net/nodebucket?retryWrites=true&w=majority";
 
 /**
  * Database connection
  */
-mongoose.connect(conn, {
-  promiseLibrary: require('bluebird'),
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-}).then(() => {
-  console.debug(`Connection to the database instance was successful`);
-}).catch(err => {
-  console.log(`MongoDB Error: ${err.message}`)
-}); // end mongoose connection
+mongoose
+  .connect(conn, {
+    promiseLibrary: require("bluebird"),
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.debug(`Connection to the database instance was successful`);
+  })
+  .catch((err) => {
+    console.log(`MongoDB Error: ${err.message}`);
+  }); // end mongoose connection
 
 /**
  * API(s) go here...
  */
 
+//app.get('/api/employees/:empId', async (req, res)) => {
+
+//}
+
 /**
  * Create and start server
  */
-http.createServer(app).listen(port, function() {
-  console.log(`Application started and listening on port: ${port}`)
+http.createServer(app).listen(port, function () {
+  console.log(`Application started and listening on port: ${port}`);
 }); // end http create server function
